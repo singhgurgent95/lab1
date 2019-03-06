6889e3bafad0bcbea9fe5bfecb18969a0debeed1
visualstudio <visualstudio@122-PC39>
2019-03-06 11:14:26 -0500

    lab1

--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// student name Gurgent Singh
+// student Id C0730067
+// Csd3354 Section 2
+//Assignment 2
+// march 6,2019
+namespace DelegatesAndEvents
+{
+    public class DelegateExercises
+    {
+        public delegate void MyDelegate();
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
 
-namespace ConsoleApp15
+    }
+}
+
+namespace DelegatesAndEvents
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
         }
     }
 }
